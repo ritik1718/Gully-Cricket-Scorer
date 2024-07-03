ef622b97ce3a7bbd5d2993ada3205db431d09711
ritik1718 <160327574+ritik1718@users.noreply.github.com>
2024-07-04 00:17:02 +0530

    Update ThisOver.jsx

--- src/components/ThisOver.jsx
+++ src/components/ThisOver.jsx
@@ -5,7 +5,7 @@ export default function ThisOver(props) {
           <div className='container w-auto my-3  rounded-md bg-blue-100 p-3'>
              <div> <h1 className=''>This Over:</h1> </div> 
             <div className='this-over flex  my-4'> {props.divs}</div>
-            <div className='decission-checkboxes flex justify-between font-semibold'>
+            <div className={`decission-checkboxes flex justify-between font-semibold ${props.hide}`}>
             <div><h3>Wide</h3>
             <input type='checkbox'onChange={props.wide} onClick={props.wide} checked={props.tick1}></input></div>
             <div><h3>No Ball</h3>
