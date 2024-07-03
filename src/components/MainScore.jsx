b8f697144e2e220c63261b1e5fee69aaa4e400b6
ritik1718 <160327574+ritik1718@users.noreply.github.com>
2024-07-04 00:17:46 +0530

    Update MainScore.jsx

--- src/components/MainScore.jsx
+++ src/components/MainScore.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function MainScore(props) {
   return (
-    <div className='container w-auto my-3  rounded-md bg-blue-200 bg-opacity-50 text-black  p-3'>
+    <div className={`container w-auto my-3  rounded-md bg-blue-200 bg-opacity-50 ${props.textcolor}  p-3`}>
     <div className='flex font-serif justify-between'>
       <div className='w-1/2 overflow-hidden'><h5>{props.batteam}</h5></div>  
       <h5>{props.inning}</h5></div>
