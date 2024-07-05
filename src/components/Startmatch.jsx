2705d3c90693487ef011e8b1213aa413f1e242aa
ritik1718 <160327574+ritik1718@users.noreply.github.com>
2024-07-05 16:15:42 +0530

    Update Startmatch.jsx

--- src/components/Startmatch.jsx
+++ src/components/Startmatch.jsx
@@ -7,18 +7,18 @@ export default function Startmatch(props) {
     <>
     <div className='teams w-auto my-3 rounded-md bg-blue-100 '>
      <h3 className=' font-bold text-3xl mx-3'  > Batting Team Name</h3>
-     <input type='text' placeholder='India' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change1} ></input> 
+     <input type='text' placeholder='Baba' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change1} ></input> 
 
      <h3 className=' font-bold text-3xl mx-3' > Bowling Team Name</h3>
-     <input type='text' placeholder='Pakistan' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change2}></input> 
+     <input type='text' placeholder='Bauaa' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change2}></input> 
      <h3 className=' font-bold text-3xl mx-3' > Bats Man 1</h3>
-     <input type='text' placeholder='Rohit Sharma' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change4}></input>
+     <input type='text' placeholder='Sundram Bhaiya' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change4}></input>
     
      <h3 className=' font-bold text-3xl mx-3' > Bats Man 2</h3>
-     <input type='text' placeholder='Virat Kohli' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change5}></input>
+     <input type='text' placeholder='Adarsh bhaiya' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change5}></input>
 
      <h3 className=' font-bold text-3xl mx-3' > Bowler </h3>
-     <input type='text' placeholder='Bumrah' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change6}></input>
+     <input type='text' placeholder='Rudra' className='w-3/4 p-3 my-4 mx-3 rounded-md' onChange={props.change6}></input>
    { /* <h3 className=' font-bold text-3xl mx-3'> No of Overs?</h3>
      <input type='number' placeholder='20' className='w-1/4 p-3 my-4 mx-3 rounded-md' onChange={props.change3}></input> */}
     </div> 
